8acc57c4e844237e1bace9dcb422f5faa262a267
Evgenia Ivleva <valkiriya-one@mail.ru>
2024-08-03 15:56:32 +0300

    finished

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Проектная работа Mesto
-
+https://github.com/Evgenia2206/mesto-project-ff.git
--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,23 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const cardTemplate = document.querySelector('#card-template').content;
+const places = document.querySelector('.places__list');
+
+initialCards.forEach(function (element) {
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+
+    cardElement.querySelector('.card__image').src = element.link;
+    cardElement.querySelector('.card__title').textContent = element.name;
+
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+
+    deleteButton.addEventListener('click', function() {
+        const cardElementDel = deleteButton.closest('.card');
+        cardElementDel.remove();
+    });
+
+    places.append(cardElement)
+});
+
